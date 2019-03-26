c44129b3ff0f3d654c05825926c84f24a6a360d8
dharmika <dharmikasharma@gmail.com>
2019-03-26 14:06:28 -0400

    login form in good shape

--- src/components/LoginComponent/loginPage.js
+++ src/components/LoginComponent/loginPage.js
@@ -1,11 +1,58 @@
 import React, { Component } from "react";
 import "./loginPage.scss";
+import { Form, Button } from "react-bootstrap";
 class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { submitted: false };
+  }
+  submitForm = () => {
+    this.setState({ submitted: true });
+  };
   render() {
+    const { submitted } = this.state;
     return (
-      <div className="Login-page">
-        hello
-        <h1>Thummuri </h1>
+      <div className="parent">
+        {!submitted && (
+          <div className="Login-page">
+            <Form onSubmit={this.submitForm}>
+              <div className="email-input">
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    className="email-text"
+                    type="email"
+                    placeholder="Enter email"
+                  />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+              </div>
+
+              <div className="password-input">
+                <Form.Group controlId="formBasicpassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    className="password"
+                    type="password"
+                    placeholder="Enter Password"
+                  />
+                  <Form.Text className="text-muted">
+                    Password should contain at leass one uppercase
+                    <br /> letter and one special character.
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="login-button">
+                <Button variant="primary" type="submit">
+                  Login
+                </Button>
+              </div>
+            </Form>
+          </div>
+        )}
+        {submitted && <h1> successfully sumbitted</h1>}
       </div>
     );
   }
