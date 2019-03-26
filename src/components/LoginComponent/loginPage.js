bb146e01551a10ea607bce7105b0f7355025802c
dharmika <dharmikasharma@gmail.com>
2019-03-26 08:44:54 -0400

    setup a few base components

---
+++ src/components/LoginComponent/loginPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./loginPage.scss";
+class LoginPage extends Component {
+  render() {
+    return <div className="Login-page" />;
+  }
+}
+
+export default LoginPage;
---
+++ src/components/NavBarComponent/navBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./navBar.scss";
+class NavBar extends Component {
+  render() {
+    return <div className="nav-bar-grid" />;
+  }
+}
+
+export default NavBar;
