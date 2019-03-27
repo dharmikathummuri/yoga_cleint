790e22a0448598326e8351ad12e76b1aa9a721e6
dharmika <dharmikasharma@gmail.com>
2019-03-27 16:32:54 -0400

    connect to react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import "./App.scss";
 import "./index.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginPage from "./components/LoginComponent";
-import Header from "./components/HeaderComponent/Header";
+import ConnectedHeader from "./components/HeaderComponent/Header";
+import Register from "./components/RegisterComponent/Register";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <div className="body">
-          <LoginPage />
-        </div>
+      <div>
+        <ConnectedHeader />
+        <Router>
+          <Switch>
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </Router>
       </div>
     );
   }
