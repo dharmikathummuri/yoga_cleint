24046e0683bdbc8dfbfaf2cb88ad491316c61c4f
dharmika <dharmikasharma@gmail.com>
2019-03-28 18:21:46 -0400

    add redux dev tools extesnion

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 export default function configureStore() {
-  return createStore(rootReducer, applyMiddleware(thunk));
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 }
